be2a2d07cc86fa95540ab4191f6d904a6bddf284
norah <norah@192.168.1.31>
2023-02-28 09:57:50 +0300

    first project commit

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ba86d2a0-a0f5-4b80-abf5-d38ed0f48275</testSuiteGuid>
+   <testCaseLink>
+      <guid>dc15ab82-c084-4d83-bc6b-e61180179ced</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DataDrivenTest</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>ea0fed1d-d23e-48d8-a1b4-5f68cc513394</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/TestData</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>ea0fed1d-d23e-48d8-a1b4-5f68cc513394</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>userName</value>
+         <variableId>7d03ba50-5cd5-4e26-8dcc-9d0bfa8b80f6</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
